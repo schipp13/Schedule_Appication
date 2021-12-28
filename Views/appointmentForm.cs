13de53fff92c9f776776a8541bc858f0b5f47a31
Saige Chipp <42128866+schipp13@users.noreply.github.com>
2021-12-28 06:43:15 -0700

    Update appointmentForm.cs

--- Views/appointmentForm.cs
+++ Views/appointmentForm.cs
@@ -1,4 +1,4 @@
-﻿using Scheduling_App.Models;
+using Scheduling_App.Models;
 using Scheduling_Application.Controller;
 using System;
 using System.IO;
@@ -39,20 +39,26 @@ namespace Scheduling_Application.Views
             this.Close();
         }
 
-        public void appointmentInfoValidator()
+        public bool appointmentInfoValidator()
         {
+            bool valid = true;
             if (String.IsNullOrEmpty(titleTextBox.Text))
             {
                 MessageBox.Show("Please enter a title for the appointment");
+                valid = false;
             }
             if (String.IsNullOrEmpty(customerComboBox.Text))
             {
                 MessageBox.Show("Please select or enter a customer name for the appointment");
+                valid = false;
             }
             if (String.IsNullOrEmpty(typeComboBox.Text))
             {
                 MessageBox.Show("Please select or enter a type for the appointments");
+                valid = false;
             }
+
+            return valid;
         }
 
         private void applyButton_Click(object sender, EventArgs e)
@@ -61,26 +67,20 @@ namespace Scheduling_Application.Views
             string formatted = "yyyy-MM-dd hh:mm:ss";
             bool found = false;
             bool withinBusinessHours = false;
+            bool validFields = appointmentInfoValidator();
 
-            appointmentInfoValidator();
-
-            // Get the customerId
-            customerId = Convert.ToInt32(dbController.getValue($"SELECT customerId FROM customer WHERE customerName = '{customerComboBox.Text}'"));
-
-            // Get the userId
-            userId = Convert.ToInt32(dbController.getValue($"SELECT userId FROM user WHERE userName = '{currentUser}'"));
-
-            // Check to see if appointment is within buisness hours
-            withinBusinessHours = validHours(startTimeDatePicker.Value, endTimeDatePicker.Value);
-            if (withinBusinessHours == true)
+            if (validFields)
             {
-                // Check if appointment exists between start and end time
-                if (startTimeDatePicker.Value.TimeOfDay < TimeSpan.Parse(minTimeValue) ||
-                startTimeDatePicker.Value.TimeOfDay > TimeSpan.Parse(maxTimeValue) || endTimeDatePicker.Value.TimeOfDay > TimeSpan.Parse(maxTimeValue) || found == true)
-                {
-                    MessageBox.Show("The appointment already exists");
-                }
-                else
+                // Get the customerId
+                customerId = Convert.ToInt32(dbController.getValue($"SELECT customerId FROM customer WHERE customerName = '{customerComboBox.Text}'"));
+
+                // Get the userId
+                userId = Convert.ToInt32(dbController.getValue($"SELECT userId FROM user WHERE userName = '{currentUser}'"));
+
+
+                // Check to see if appointment is within buisness hours
+                withinBusinessHours = validHours(startTimeDatePicker.Value, endTimeDatePicker.Value);
+                if (withinBusinessHours == true)
                 {
                     // Combine the start date and time
                     startDay = startDateTimePicker.Value.Date;
@@ -92,81 +92,86 @@ namespace Scheduling_Application.Views
                     endTime = endTimeDatePicker.Value.TimeOfDay;
                     combinedEndDateTime = endDay + endTime;
 
-                    // Check to see if appointment exists at appointmentId
-                    found = dbController.existQuery($"SELECT * FROM appointment WHERE appointmentId = '{appointmentId}'");
-
+                    found = dbController.existQuery($"SELECT * FROM appointment WHERE start = '{combinedStartDateTime.ToUniversalTime().ToString(formatted)}'");
+                    // Check if appointment exists
                     if (found == true)
                     {
-                        // Make changes to the appointment
-                        appointment updatedAppointment = new appointment();
-
-                        updatedAppointment.title = titleTextBox.Text;
-                        updatedAppointment.customerId = customerId;
-                        updatedAppointment.userId = userId;
-                        updatedAppointment.description = descriptionTextBox.Text;
-                        updatedAppointment.location = locationTextBox.Text;
-                        updatedAppointment.contact = contactComboBox.Text;
-                        updatedAppointment.type = typeComboBox.Text;
-                        updatedAppointment.url = urlTextBox.Text;
-                        updatedAppointment.start = combinedStartDateTime;
-                        updatedAppointment.end = combinedEndDateTime;
-                        updatedAppointment.lastUpdate = DateTime.Now;
-                        updatedAppointment.lastUpdateBy = currentUser;
-
-                        dbController.updateQuery($"UPDATE appointment SET customerId = '{updatedAppointment.customerId}', userId = '{updatedAppointment.userId}', title = '{updatedAppointment.title}'," +
-                            $" description = '{updatedAppointment.description}', location = '{updatedAppointment.location}', contact= '{updatedAppointment.contact}', type = '{updatedAppointment.type}'," +
-                            $" url = '{updatedAppointment.url}', start = '{updatedAppointment.start.ToUniversalTime().ToString(formatted)}', end = '{updatedAppointment.end.ToUniversalTime().ToString(formatted)}', lastUpdate = '{updatedAppointment.lastUpdate.ToString(formatted)}', lastUpdateBy = '{updatedAppointment.lastUpdateBy}'" +
-                            $" WHERE appointmentId = {appointmentId}");
-
-                        MessageBox.Show("The appointment was updated");
-                        // append to file
-                        using (StreamWriter writer = File.AppendText(path))
-                        {
-                            writer.WriteLine($"{currentUser} updated {updatedAppointment.title} at {DateTime.Now.ToLocalTime()}");
-                        }
+                        MessageBox.Show("The appointment already exists");
                     }
                     else
                     {
-                        //Create new appointment
-                        appointment newAppointment = new appointment();
 
-                        newAppointment.title = titleTextBox.Text;
-                        newAppointment.customerId = customerId;
-                        newAppointment.userId = userId;
-                        newAppointment.description = descriptionTextBox.Text;
-                        newAppointment.location = locationTextBox.Text;
-                        newAppointment.contact = contactComboBox.Text;
-                        newAppointment.type = typeComboBox.Text;
-                        newAppointment.url = urlTextBox.Text;
-                        newAppointment.start = combinedStartDateTime;
-                        newAppointment.end = combinedEndDateTime;
-                        newAppointment.createDate = DateTime.Now;
-                        newAppointment.createdBy = currentUser;
-                        newAppointment.lastUpdate = DateTime.Now;
-                        newAppointment.lastUpdateBy = currentUser;
-
-                        // Insert appointment into Database
-                        dbController.insertQuery($"INSERT INTO appointment (customerId, userId, title, description, location, contact, type, url, start, end, createDate, createdBy, lastUpdate, lastUpdateBy) VALUES " +
-                               $" ('{newAppointment.customerId}', '{newAppointment.userId}', '{newAppointment.title}', '{newAppointment.description}', " +
-                               $"'{newAppointment.location}', '{newAppointment.contact}', '{newAppointment.type}', '{newAppointment.url}', '{newAppointment.start.ToUniversalTime().ToString(formatted)}', '{newAppointment.end.ToUniversalTime().ToString(formatted)}'," +
-                               $"'{ newAppointment.createDate.ToString(formatted)}', '{newAppointment.createdBy}', '{newAppointment.lastUpdate.ToString(formatted)}', '{newAppointment.lastUpdateBy}')");
-
-
-                        MessageBox.Show("The appointment was created");
-
-                        using (StreamWriter writer = File.AppendText(path))
+                        if (editMode == true)
                         {
-                            writer.WriteLine($"{currentUser} created {newAppointment.title} at {DateTime.Now.ToLocalTime()}");
+                            // Make changes to the appointment
+                            appointment updatedAppointment = new appointment();
+
+                            updatedAppointment.title = titleTextBox.Text;
+                            updatedAppointment.customerId = customerId;
+                            updatedAppointment.userId = userId;
+                            updatedAppointment.description = descriptionTextBox.Text;
+                            updatedAppointment.location = locationTextBox.Text;
+                            updatedAppointment.contact = contactComboBox.Text;
+                            updatedAppointment.type = typeComboBox.Text;
+                            updatedAppointment.url = urlTextBox.Text;
+                            updatedAppointment.start = combinedStartDateTime;
+                            updatedAppointment.end = combinedEndDateTime;
+                            updatedAppointment.lastUpdate = DateTime.Now;
+                            updatedAppointment.lastUpdateBy = currentUser;
+
+                            dbController.updateQuery($"UPDATE appointment SET customerId = '{updatedAppointment.customerId}', userId = '{updatedAppointment.userId}', title = '{updatedAppointment.title}'," +
+                                $" description = '{updatedAppointment.description}', location = '{updatedAppointment.location}', contact= '{updatedAppointment.contact}', type = '{updatedAppointment.type}'," +
+                                $" url = '{updatedAppointment.url}', start = '{updatedAppointment.start.ToUniversalTime().ToString(formatted)}', end = '{updatedAppointment.end.ToUniversalTime().ToString(formatted)}', lastUpdate = '{updatedAppointment.lastUpdate.ToString(formatted)}', lastUpdateBy = '{updatedAppointment.lastUpdateBy}'" +
+                                $" WHERE appointmentId = {appointmentId}");
+
+                            MessageBox.Show("The appointment was updated");
+                            // append to file
+                            using (StreamWriter writer = File.AppendText(path))
+                            {
+                                writer.WriteLine($"{currentUser} updated {updatedAppointment.title} at {DateTime.Now.ToLocalTime()}");
+                            }
+
                         }
+                        else
+                        {
+                            //Create new appointment
+                            appointment newAppointment = new appointment();
 
+                            newAppointment.title = titleTextBox.Text;
+                            newAppointment.customerId = customerId;
+                            newAppointment.userId = userId;
+                            newAppointment.description = descriptionTextBox.Text;
+                            newAppointment.location = locationTextBox.Text;
+                            newAppointment.contact = contactComboBox.Text;
+                            newAppointment.type = typeComboBox.Text;
+                            newAppointment.url = urlTextBox.Text;
+                            newAppointment.start = combinedStartDateTime;
+                            newAppointment.end = combinedEndDateTime;
+                            newAppointment.createDate = DateTime.Now;
+                            newAppointment.createdBy = currentUser;
+                            newAppointment.lastUpdate = DateTime.Now;
+                            newAppointment.lastUpdateBy = currentUser;
+
+                            // Insert appointment into Database
+                            dbController.insertQuery($"INSERT INTO appointment (customerId, userId, title, description, location, contact, type, url, start, end, createDate, createdBy, lastUpdate, lastUpdateBy) VALUES " +
+                                   $" ('{newAppointment.customerId}', '{newAppointment.userId}', '{newAppointment.title}', '{newAppointment.description}', " +
+                                   $"'{newAppointment.location}', '{newAppointment.contact}', '{newAppointment.type}', '{newAppointment.url}', '{newAppointment.start.ToUniversalTime().ToString(formatted)}', '{newAppointment.end.ToUniversalTime().ToString(formatted)}'," +
+                                   $"'{ newAppointment.createDate.ToString(formatted)}', '{newAppointment.createdBy}', '{newAppointment.lastUpdate.ToString(formatted)}', '{newAppointment.lastUpdateBy}')");
+
+                            MessageBox.Show("The appointment was created");
+
+                            using (StreamWriter writer = File.AppendText(path))
+                            {
+                                writer.WriteLine($"{currentUser} created {newAppointment.title} at {DateTime.Now.ToLocalTime()}");
+                            }
+                        }
                     }
-
+                }
+                else
+                {
+                    MessageBox.Show("The appointment times are not within the valid hours. (8 am - 5 pm)");
                 }
 
-            }
-            else
-            {
-                MessageBox.Show("The appointment times are not within the valid hours. (8 am - 5 pm)");
             }
         }
 
@@ -224,6 +229,7 @@ namespace Scheduling_Application.Views
                                            type = appointment.type,
                                            url = appointment.url,
 
+
                                        };
 
                 foreach (var appt in appointmentQuery)
@@ -248,7 +254,6 @@ namespace Scheduling_Application.Views
 
                 start = dbController.getValue($"SELECT start FROM appointment WHERE appointmentId = '{appointmentId}'");
                 end = dbController.getValue($"SELECT end FROM appointment WHERE appointmentId = '{appointmentId}'");
-
 
                 startDateTimeSplit = start.Split(' ');
                 endDateTimeSplit = end.Split(' ');
